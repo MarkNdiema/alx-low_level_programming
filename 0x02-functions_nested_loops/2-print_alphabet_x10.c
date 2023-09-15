bdfdf8350171145fcd883f29b70b34a12f6418e0
MarkNdiema <markndiema@gmail.com>
2023-09-14 21:20:10 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,20 @@
-#include <studio.h>
 #include "main.h"
+
 /**
   * print_alphabet_x10 - Prints the alphabet
   */
 void print_alphabet_x10(void)
 {
-	char n;
-	int i;
-	for (i = 0; i < 10; i++)
+	char c;
+	int i=0;
+
+	while (i <= 9)
 	{
-		for (n = 'a'; n <= 'z'; n++)
-		{
-			putchar(n);
-		}
-		putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
