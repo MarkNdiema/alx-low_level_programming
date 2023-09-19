04a6763ba0d6757862da40713db12edab357fc05
MarkNdiema <markndiema@gmail.com>
2023-09-19 01:32:33 -0700

    0-preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -9,7 +9,7 @@ int main(void)
 {
     return (0);
 }
-julien@ubuntu:~/c/0x00$ export CFILE=main.c
+julien@ubuntu:~/c/0x00$ export CFILE=0-main.c
 julien@ubuntu:~/c/0x00$ ./0-preprocessor 
 julien@ubuntu:~/c/0x00$ tail c
 # 942 "/usr/include/stdio.h" 3 4
