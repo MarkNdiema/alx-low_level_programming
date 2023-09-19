89be0394310659b52f5ca32b477e4c3083137f79
MarkNdiema <markndiema@gmail.com>
2023-09-19 04:07:47 -0700

    Hello,printf"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - E ntey point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+prtintf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
