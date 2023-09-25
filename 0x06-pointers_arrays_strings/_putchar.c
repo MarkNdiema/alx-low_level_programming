83a54ef295fb065e1e886d3f21c22630110acbb2
MarkNdiema <markndiema@gmail.com>
2023-09-25 01:46:34 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - Writes a character to the standard output (stdout).
- * @c: The character to be written.
+ * _putchar - Writes a character to standard output
+ * @c: The character to write
  *
- * Return: On success, returns the character written. On error, returns -1.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-    return write(1, &c, 1);  /* File descriptor 1 represents stdout */
+    return write(1, &c, 1);
 }
