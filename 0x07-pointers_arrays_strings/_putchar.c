5d34e017f714f6a26ef29c4e963a613817ba3bce
MarkNdiema <markndiema@gmail.com>
2023-09-26 01:57:29 -0700

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "add.h"  // Include the custom header file
+
+int main() {
+    int result = add(5, 3); // Use the "add" function
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
